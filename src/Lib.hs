23f4789cf4d958e8cd5d5ca7c0e11ad67f1dd8db
Matt Teichman <teichman@uchicago.edu>
2024-09-30 20:41:45 -0500

    put my dumb transpose definition in

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,12 @@
 module Lib where
 
+import Data.List (transpose)
+
 type Matrix a = [[a]]
+
+-- not necessary since the version in Data.List is better, just wrote
+-- this for fun
+transpose' :: Matrix a -> Matrix a
+transpose' arr =
+  [ arr >>= drop x . take (x + 1)
+  | x <- [0..(maximum (map length arr) - 1)] ]
