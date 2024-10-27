9f538bb9a207e92c665719014f2a8bdc2643924d
Matt Teichman <teichman@uchicago.edu>
2024-10-27 12:32:05 -0500

    change zeroCompPred to zeroComp

--- src/Lib.hs
+++ src/Lib.hs
@@ -32,27 +32,11 @@ get row col mx = do
 get' :: (Int, Int) -> Matrix a -> Maybe a
 get' = uncurry get
 
-zeroCompPred :: (Num a, Eq a) => [a] -> [a] -> Bool
-zeroCompPred lst1 lst2 =
+zeroComp :: (Num a, Eq a) => [a] -> [a] -> Bool
+zeroComp lst1 lst2 =
   let zilch = (== 0)
       zeroCount = length . takeWhile zilch
   in zeroCount lst2 > zeroCount lst1
-
-findWithIndex :: (a -> Bool) -> [a] -> Maybe (Int, a)
-findWithIndex pred lst =
-  let findWithIndex' _ _ [] = Nothing
-      findWithIndex' idx pred (x : xs) =
-        if pred x
-        then Just (idx, x)
-        else findWithIndex' (idx + 1) pred xs
-  in findWithIndex' 0 pred lst
-
-insertByIndex :: Int -> a -> [a] -> [a]
-insertByIndex _ _ [] = []
-insertByIndex idx _ lst | idx < 1 = lst
-insertByIndex 1 item (x : xs) = x : item : xs
-insertByIndex idx item (x : xs) =
-  x : insertByIndex (idx - 1) item xs
 
 swapByIndex :: Eq a => Int -> [a] -> [a]
 swapByIndex idx [] = []
